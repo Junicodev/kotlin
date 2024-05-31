ab00e803b100709c89501a061dd24009ee4a7086
Juan Lopez Vallejo <juanmi.lopez@gruposantander.com>
2024-05-31 07:47:36 +0200

    Launch menu

--- app/src/main/java/com/junicodev/androidmaster/MenuActivity.kt
+++ app/src/main/java/com/junicodev/androidmaster/MenuActivity.kt
@@ -1,11 +1,27 @@
 package com.junicodev.androidmaster
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.junicodev.androidmaster.firstApp.FirstAppActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        btnSaludApp.setOnClickListener { navigateToSaludApp() }
+        val btnIMCApp = findViewById<Button>(R.id.btnIMCApp)
+        btnSaludApp.setOnClickListener { navigateToIMCApp() }
+    }
+
+    private fun navigateToSaludApp() {
+        val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToIMCApp() {
+
     }
 }
---
+++ app/src/main/java/com/junicodev/androidmaster/imccalculator/imcCalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.junicodev.androidmaster.imccalculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.junicodev.androidmaster.R
+
+class imcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+    }
+}
