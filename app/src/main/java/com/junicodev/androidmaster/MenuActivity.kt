b14ecc7a06f5fa7f1f724c2044838093955e60a6
Juan Lopez Vallejo <juanmi.lopez@gruposantander.com>
2024-06-03 07:58:59 +0200

    Start imcLayout

--- app/src/main/java/com/junicodev/androidmaster/MenuActivity.kt
+++ app/src/main/java/com/junicodev/androidmaster/MenuActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.junicodev.androidmaster.firstApp.FirstAppActivity
+import com.junicodev.androidmaster.imccalculator.imcCalculatorActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +23,7 @@ class MenuActivity : AppCompatActivity() {
     }
 
     private fun navigateToIMCApp() {
-
+        val intent = Intent(this, imcCalculatorActivity::class.java)
+        startActivity(intent)
     }
 }
