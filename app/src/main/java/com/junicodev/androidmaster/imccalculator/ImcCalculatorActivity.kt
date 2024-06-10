58cc752bd715f5cf2464ec7f67efc7cbe37cddb8
Juan López <junicodevv@gmail.com>
2024-06-10 07:32:37 +0200

    Set age

--- app/src/main/java/com/junicodev/androidmaster/imccalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/junicodev/androidmaster/imccalculator/ImcCalculatorActivity.kt
@@ -5,6 +5,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import com.junicodev.androidmaster.R
 import java.text.DecimalFormat
@@ -15,8 +16,16 @@ class ImcCalculatorActivity : AppCompatActivity() {
     private lateinit var viewFemale: CardView
     private lateinit var tvHeight: TextView
     private lateinit var rsHeight: RangeSlider
+    private lateinit var btnSubtractWeight: FloatingActionButton
+    private lateinit var btnAddWeight: FloatingActionButton
+    private lateinit var tvWeight: TextView
+    private lateinit var btnSubtractAge: FloatingActionButton
+    private lateinit var btnAddAge: FloatingActionButton
+    private lateinit var tvAge: TextView
 
     private var isMaleSelected: Boolean = true
+    private var currentWeight: Int = 60
+    private var currentAge: Int = 30
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +40,12 @@ class ImcCalculatorActivity : AppCompatActivity() {
         viewFemale = findViewById(R.id.viewFemale)
         tvHeight = findViewById(R.id.tvHeight)
         rsHeight = findViewById(R.id.rsHeight)
+        btnSubtractWeight = findViewById(R.id.btnSubtractWeight)
+        btnAddWeight = findViewById(R.id.btnAddWeight)
+        tvWeight = findViewById(R.id.tvWeight)
+        btnSubtractAge = findViewById(R.id.btnSubtractAge)
+        btnAddAge = findViewById(R.id.btnAddAge)
+        tvAge = findViewById(R.id.tvAge)
     }
 
     private fun initListeners() {
@@ -42,7 +57,33 @@ class ImcCalculatorActivity : AppCompatActivity() {
             isMaleSelected = false
             setGenderColor()
         }
-        rsHeight.addOnChangeListener { _, value, _ -> tvHeight.text = DecimalFormat("#.##").format(value) + " " + getString(R.string.unit) }
+        rsHeight.addOnChangeListener { _, value, _ ->
+            tvHeight.text = DecimalFormat("#.##").format(value) + " " + getString(R.string.unit)
+        }
+        btnSubtractWeight.setOnClickListener {
+            currentWeight--
+            setWeight()
+        }
+        btnAddWeight.setOnClickListener {
+            currentWeight++
+            setWeight()
+        }
+        btnSubtractAge.setOnClickListener {
+            currentAge--
+            setAge()
+        }
+        btnAddAge.setOnClickListener {
+            currentAge++
+            setAge()
+        }
+    }
+
+    private fun setWeight() {
+        tvWeight.text = currentWeight.toString()
+    }
+
+    private fun setAge() {
+        tvAge.text = currentAge.toString()
     }
 
     private fun setGenderColor() {
@@ -50,12 +91,17 @@ class ImcCalculatorActivity : AppCompatActivity() {
         viewFemale.setBackgroundColor(getBackgroundColor(!isMaleSelected))
     }
 
-    private fun getBackgroundColor(isSelectedComponent:Boolean): Int {
-        return ContextCompat.getColor(this, if(isSelectedComponent) R.color.background_component_selected else R.color.background_component)
+    private fun getBackgroundColor(isSelectedComponent: Boolean): Int {
+        return ContextCompat.getColor(
+            this,
+            if (isSelectedComponent) R.color.background_component_selected else R.color.background_component
+        )
     }
 
     private fun initUI() {
         setGenderColor()
+        setWeight()
+        setAge()
     }
 
 }
