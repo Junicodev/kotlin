94cea09a3e301194bfb22de4f30c2c210db22929
Juan López <junicodevv@gmail.com>
2024-06-11 08:00:02 +0200

    Add layout weight

--- app/src/main/java/com/junicodev/androidmaster/imccalculator/ResultIMCActivity.kt
+++ app/src/main/java/com/junicodev/androidmaster/imccalculator/ResultIMCActivity.kt
@@ -1,14 +1,68 @@
 package com.junicodev.androidmaster.imccalculator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.junicodev.androidmaster.R
 import com.junicodev.androidmaster.imccalculator.ImcCalculatorActivity.Companion.IMC_KEY
 
 class ResultIMCActivity : AppCompatActivity() {
+
+    private lateinit var tvResult:TextView
+    private lateinit var tvIMC:TextView
+    private lateinit var tvDescription:TextView
+    private lateinit var btnRecalculate:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result_imcactivity)
         val result:String = intent.extras?.getString(IMC_KEY) ?: "-1.0"
+        initComponents()
+        initUI(result)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        btnRecalculate.setOnClickListener { onBackPressed() }
+    }
+
+    private fun initUI(result:String) {
+        tvIMC.text = result
+        when (result.toDouble()) {
+            in 0.00..18.50 -> {
+                tvResult.text = getString(R.string.title_low_weight)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.low_weight))
+                tvDescription.text = getString(R.string.description_low_weight)
+            }
+            in 18.51..24.99 -> {
+                tvResult.text = getString(R.string.title_normal_weight)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.normal_weight))
+                tvDescription.text = getString(R.string.description_normal_weight)
+            }
+            in 25.00..29.99 -> {
+                tvResult.text = getString(R.string.title_high_weight)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.high_weight))
+                tvDescription.text = getString(R.string.description_high_weight)
+            }
+            in 30.00..99.99 -> {
+                tvResult.text = getString(R.string.title_overweight)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.overweight))
+                tvDescription.text = getString(R.string.description_overweight)
+            }
+            else -> {
+                tvIMC.text = getString(R.string.error)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.overweight))
+                tvResult.text = getString(R.string.error)
+                tvDescription.text= getString(R.string.error)
+            }
+        }
+    }
+
+    private fun initComponents() {
+        tvResult = findViewById(R.id.tvResult)
+        tvIMC = findViewById(R.id.tvIMC)
+        tvDescription = findViewById(R.id.tvDescription)
+        btnRecalculate = findViewById(R.id.btnRecalculate)
     }
 }
