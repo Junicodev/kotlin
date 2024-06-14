42142e396130282fd6555eb8a42ab7a67035d2f4
Juan López <junicodevv@gmail.com>
2024-06-14 07:48:49 +0200

    Start reciclerView

---
+++ app/src/main/java/com/junicodev/androidmaster/todoapp/CategoriesViewHolder.kt
@@ -0,0 +1,8 @@
+package com.junicodev.androidmaster.todoapp
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CategoriesViewHolder(view:View) : RecyclerView.ViewHolder(view){
+
+}
--- app/src/main/java/com/junicodev/androidmaster/todoapp/TodoActivity.kt
+++ app/src/main/java/com/junicodev/androidmaster/todoapp/TodoActivity.kt
@@ -2,11 +2,24 @@ package com.junicodev.androidmaster.todoapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.junicodev.androidmaster.R
 
 class TodoActivity : AppCompatActivity() {
+
+    private lateinit var rvCategories:RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
+        initComponent()
+        initUI()
+    }
+
+    private fun initComponent() {
+        rvCategories = findViewById(R.id.rvCategories)
+    }
+
+    private fun initUI() {
+
     }
 }
