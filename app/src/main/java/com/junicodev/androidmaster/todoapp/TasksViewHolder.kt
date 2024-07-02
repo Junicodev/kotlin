e4933d074e07e8a247b3e372a34d00a4a6611dc5
Juan López <junicodevv@gmail.com>
2024-07-02 07:31:38 +0200

    Add FAB button to add tasks

--- app/src/main/java/com/junicodev/androidmaster/todoapp/TasksViewHolder.kt
+++ app/src/main/java/com/junicodev/androidmaster/todoapp/TasksViewHolder.kt
@@ -3,6 +3,7 @@ package com.junicodev.androidmaster.todoapp
 import android.view.View
 import android.widget.CheckBox
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.junicodev.androidmaster.R
 
@@ -12,5 +13,33 @@ class TasksViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val cbTask: CheckBox = view.findViewById(R.id.cbTask)
     fun render(task: Task) {
         tvTask.text = task.name
+        when (task.category) {
+            TaskCategory.Business -> {
+                cbTask.setTextColor(
+                    ContextCompat.getColor(
+                        cbTask.context,
+                        R.color.todo_business_category
+                    )
+                )
+            }
+
+            TaskCategory.Other -> {
+                cbTask.setTextColor(
+                    ContextCompat.getColor(
+                        cbTask.context,
+                        R.color.todo_other_category
+                    )
+                )
+            }
+
+            TaskCategory.Personal -> {
+                cbTask.setTextColor(
+                    ContextCompat.getColor(
+                        cbTask.context,
+                        R.color.todo_personal_category
+                    )
+                )
+            }
+        }
     }
 }
