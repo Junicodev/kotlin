9a802e6c1aafeee3fcbc8329fa74a0e374a96f40
Juan López <junicodevv@gmail.com>
2024-07-02 07:51:32 +0200

    Add dialog

--- app/src/main/java/com/junicodev/androidmaster/todoapp/TodoActivity.kt
+++ app/src/main/java/com/junicodev/androidmaster/todoapp/TodoActivity.kt
@@ -1,9 +1,11 @@
 package com.junicodev.androidmaster.todoapp
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.junicodev.androidmaster.R
 import com.junicodev.androidmaster.todoapp.TaskCategory.Business
 import com.junicodev.androidmaster.todoapp.TaskCategory.Other
@@ -23,27 +25,42 @@ class TodoActivity : AppCompatActivity() {
         Task("TestOther", Other),
     )
 
-    private lateinit var rvCategories:RecyclerView
-    private lateinit var categoriesAdapter:CategoriesAdapter
+    private lateinit var rvCategories: RecyclerView
+    private lateinit var categoriesAdapter: CategoriesAdapter
 
-    private lateinit var rvTasks:RecyclerView
+    private lateinit var rvTasks: RecyclerView
     private lateinit var tasksAdapter: TasksAdapter
+
+    private lateinit var fabAddTask: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
         initComponent()
         initUI()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        fabAddTask.setOnClickListener { showDialog() }
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_task)
+        dialog.show()
     }
 
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
         rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.fabAddTask)
     }
 
     private fun initUI() {
         categoriesAdapter = CategoriesAdapter(categories)
-        rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvCategories.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
 
         tasksAdapter = TasksAdapter(tasks)
