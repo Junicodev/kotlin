78823c5bda8aa7834329a061e67d97d66d70f66a
Juan López <junicodevv@gmail.com>
2024-07-09 07:10:58 +0200

    Add retrofit

--- app/src/main/java/com/junicodev/androidmaster/superheroapp/SuperHeroListActivity.kt
+++ app/src/main/java/com/junicodev/androidmaster/superheroapp/SuperHeroListActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import com.junicodev.androidmaster.databinding.ActivitySuperHeroListBinding
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SuperHeroListActivity : AppCompatActivity() {
 
     // view binding
     private lateinit var binding: ActivitySuperHeroListBinding
+    private lateinit var retrofit: Retrofit
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySuperHeroListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        retrofit = getRetrofit()
         initUI()
     }
 
@@ -29,5 +34,10 @@ class SuperHeroListActivity : AppCompatActivity() {
 
     private fun searchByName(query: String) {
 
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder().baseUrl("https://superheroapi.com/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
     }
 }
